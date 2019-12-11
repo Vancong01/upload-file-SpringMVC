cc2c25b113facfc2e0b9f36fb35ab1ca650c6b08
Acnologia1 <46494938+Acnologia1@users.noreply.github.com>
2019-12-11 08:45:50 +0700

    cong

---
+++ src/main/java/com/codegym/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.codegym.repository;
+
+import com.codegym.model.Customer;
+
+import java.util.List;
+
+public interface CustomerRepository {
+    List<Customer> findAll();
+    Customer findById(Long id);
+    void save(Customer customer);
+    void remove(Long id);
+}
---
+++ src/main/java/com/codegym/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.codegym.repository;
+
+import com.codegym.model.Product;
+
+import java.util.List;
+
+public interface ProductRepository {
+    List<Product> findAll();
+    Product findById(Long id);
+    void save(Product product);
+    void remove(Long id);
+}
